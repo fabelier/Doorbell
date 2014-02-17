02fa94060d7cf7458a173f17544d7ada235df1d0
Guillaume Turri <guillaume.turri@gmail.com>
2014-02-17 23:00:34 +0100

    Can use jshint with grunt
    and fixed some warnings

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+/* global module:false */
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			options: {
+				curly: false,
+				eqeqeq: true,
+				immed: true,
+				latedef: false,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				eqnull: true,
+				browser: true,
+				expr: true,
+				globals: {
+                   __dirname: false,
+                    Buffer: false,
+                    console: false,
+					require: false
+				}
+			},
+			files: [ 'Gruntfile.js', 'index.js' ]
+		}
+	});
+
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+
+	grunt.registerTask( 'default', [ 'jshint' ] );
+};
